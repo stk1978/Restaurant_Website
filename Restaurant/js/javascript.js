1c16a94f3ec6bf3719a25e35ca7692a71d8c8dad
stylianos kouroupas <steliosk9@gmail.com>
2020-04-09 23:33:52 -0500

    invisible navabar

--- Restaurant/js/javascript.js
+++ Restaurant/js/javascript.js
@@ -1,179 +1,179 @@
-// Menu
+// // Menu
 
-const buttons = document.querySelectorAll('.button');
-const menus = document.querySelectorAll('.menu');
+// const buttons = document.querySelectorAll('.button');
+// const menus = document.querySelectorAll('.menu');
 
-const highlight = document.createElement('span');
-document.body.appendChild(highlight);
-highlight.classList.add('highlight');
+// const highlight = document.createElement('span');
+// document.body.appendChild(highlight);
+// highlight.classList.add('highlight');
 
-// Set initial dimensions and position of 'highlight' based on activeButton coords 
-function initialHightlightLocation() {
-  const activeButton = document.querySelector('.button--is-active');
-  const activeButtonCoords = activeButton.getBoundingClientRect();
+// // Set initial dimensions and position of 'highlight' based on activeButton coords 
+// function initialHightlightLocation() {
+//   const activeButton = document.querySelector('.button--is-active');
+//   const activeButtonCoords = activeButton.getBoundingClientRect();
 
-  const initialCoords = {
-    width: activeButtonCoords.width,
-    height: activeButtonCoords.height,
-    left: activeButtonCoords.left + window.scrollX,
-    top: activeButtonCoords.top + window.scrollY
-  }
+//   const initialCoords = {
+//     width: activeButtonCoords.width,
+//     height: activeButtonCoords.height,
+//     left: activeButtonCoords.left + window.scrollX,
+//     top: activeButtonCoords.top + window.scrollY
+//   }
 
-  highlight.style.width = `${initialCoords.width}px`;
-  highlight.style.height = `${initialCoords.height}px`;
-  highlight.style.transform = `translate(${initialCoords.left}px, ${initialCoords.top}px)`;
-}
+//   highlight.style.width = `${initialCoords.width}px`;
+//   highlight.style.height = `${initialCoords.height}px`;
+//   highlight.style.transform = `translate(${initialCoords.left}px, ${initialCoords.top}px)`;
+// }
 
-function handleClick(e) {
-  e.preventDefault();
+// function handleClick(e) {
+//   e.preventDefault();
 
-  buttons.forEach(button => button.classList.remove('button--is-active'));
-  this.classList.add('button--is-active');
+//   buttons.forEach(button => button.classList.remove('button--is-active'));
+//   this.classList.add('button--is-active');
 
-  // Set current dimensions and position of 'highlight' based on the clicked button 
-  const buttonCoords = this.getBoundingClientRect();
-  const coords = {
-    width: buttonCoords.width,
-    height: buttonCoords.height,
-    left: buttonCoords.left + window.scrollX,
-    top: buttonCoords.top + window.scrollY
-  }
-  highlight.style.width = `${coords.width}px`;
-  highlight.style.height = `${coords.height}px`;
-  highlight.style.transform = `translate(${coords.left}px, ${coords.top}px)`;
+//   // Set current dimensions and position of 'highlight' based on the clicked button 
+//   const buttonCoords = this.getBoundingClientRect();
+//   const coords = {
+//     width: buttonCoords.width,
+//     height: buttonCoords.height,
+//     left: buttonCoords.left + window.scrollX,
+//     top: buttonCoords.top + window.scrollY
+//   }
+//   highlight.style.width = `${coords.width}px`;
+//   highlight.style.height = `${coords.height}px`;
+//   highlight.style.transform = `translate(${coords.left}px, ${coords.top}px)`;
 
-  // Show the menu associated to the clicked button
-  const targetMenu = document.querySelector(`#${this.dataset.target}`);
-  menus.forEach(menu => {
-    menu.classList.remove('menu--is-visible');
-    targetMenu.classList.add('menu--is-visible');
-  })
-}
+//   // Show the menu associated to the clicked button
+//   const targetMenu = document.querySelector(`#${this.dataset.target}`);
+//   menus.forEach(menu => {
+//     menu.classList.remove('menu--is-visible');
+//     targetMenu.classList.add('menu--is-visible');
+//   })
+// }
 
-window.addEventListener('load', initialHightlightLocation);
-window.addEventListener('resize', initialHightlightLocation);
-buttons.forEach(button => button.addEventListener('click', handleClick));
+// window.addEventListener('load', initialHightlightLocation);
+// window.addEventListener('resize', initialHightlightLocation);
+// buttons.forEach(button => button.addEventListener('click', handleClick));
 
-// Menu end //
+// // Menu end //
 
 
 
-jQuery(document).ready(function($) {
-"use strict";
+// jQuery(document).ready(function($) {
+// "use strict";
 
-    //Contact
-    $('form.contactForm').submit(function(){
+//     //Contact
+//     $('form.contactForm').submit(function(){
 
-        var f = $(this).find('.form-group'), 
-        ferror = false, 
-        emailExp = /^[^\s()<>@,;:\/]+@\w[\w\.-]+\.[a-z]{2,}$/i;
+//         var f = $(this).find('.form-group'), 
+//         ferror = false, 
+//         emailExp = /^[^\s()<>@,;:\/]+@\w[\w\.-]+\.[a-z]{2,}$/i;
 
-        f.children('input').each(function(){ // run all inputs
+//         f.children('input').each(function(){ // run all inputs
 
-            var i = $(this); // current input
-            var rule = i.attr('data-rule');
+//             var i = $(this); // current input
+//             var rule = i.attr('data-rule');
 
-            if( rule !== undefined ){
-            var ierror=false; // error flag for current input
-            var pos = rule.indexOf( ':', 0 );
-            if( pos >= 0 ){
-                var exp = rule.substr( pos+1, rule.length );
-                rule = rule.substr(0, pos);
-            }else{
-                rule = rule.substr( pos+1, rule.length );
-            }
+//             if( rule !== undefined ){
+//             var ierror=false; // error flag for current input
+//             var pos = rule.indexOf( ':', 0 );
+//             if( pos >= 0 ){
+//                 var exp = rule.substr( pos+1, rule.length );
+//                 rule = rule.substr(0, pos);
+//             }else{
+//                 rule = rule.substr( pos+1, rule.length );
+//             }
             
-            switch( rule ){
-                case 'required':
-                if( i.val()==='' ){ ferror=ierror=true; }
-                break;
+//             switch( rule ){
+//                 case 'required':
+//                 if( i.val()==='' ){ ferror=ierror=true; }
+//                 break;
                 
-                case 'minlen':
-                if( i.val().length<parseInt(exp) ){ ferror=ierror=true; }
-                break;
+//                 case 'minlen':
+//                 if( i.val().length<parseInt(exp) ){ ferror=ierror=true; }
+//                 break;
 
-                case 'email':
-                if( !emailExp.test(i.val()) ){ ferror=ierror=true; }
-                break;
+//                 case 'email':
+//                 if( !emailExp.test(i.val()) ){ ferror=ierror=true; }
+//                 break;
 
-                case 'checked':
-                if( !i.attr('checked') ){ ferror=ierror=true; }
-                break;
+//                 case 'checked':
+//                 if( !i.attr('checked') ){ ferror=ierror=true; }
+//                 break;
                 
-                case 'regexp':
-                exp = new RegExp(exp);
-                if( !exp.test(i.val()) ){ ferror=ierror=true; }
-                break;
-            }
-                i.next('.validation').html( ( ierror ? (i.attr('data-msg') !== undefined ? i.attr('data-msg') : 'wrong Input') : '' ) ).show('blind');
-            }
-        });
-        f.children('textarea').each(function(){ // run all inputs
+//                 case 'regexp':
+//                 exp = new RegExp(exp);
+//                 if( !exp.test(i.val()) ){ ferror=ierror=true; }
+//                 break;
+//             }
+//                 i.next('.validation').html( ( ierror ? (i.attr('data-msg') !== undefined ? i.attr('data-msg') : 'wrong Input') : '' ) ).show('blind');
+//             }
+//         });
+//         f.children('textarea').each(function(){ // run all inputs
 
-            var i = $(this); // current input
-            var rule = i.attr('data-rule');
+//             var i = $(this); // current input
+//             var rule = i.attr('data-rule');
 
-            if( rule !== undefined ){
-            var ierror=false; // error flag for current input
-            var pos = rule.indexOf( ':', 0 );
-            if( pos >= 0 ){
-                var exp = rule.substr( pos+1, rule.length );
-                rule = rule.substr(0, pos);
-            }else{
-                rule = rule.substr( pos+1, rule.length );
-            }
+//             if( rule !== undefined ){
+//             var ierror=false; // error flag for current input
+//             var pos = rule.indexOf( ':', 0 );
+//             if( pos >= 0 ){
+//                 var exp = rule.substr( pos+1, rule.length );
+//                 rule = rule.substr(0, pos);
+//             }else{
+//                 rule = rule.substr( pos+1, rule.length );
+//             }
             
-            switch( rule ){
-                case 'required':
-                if( i.val()==='' ){ ferror=ierror=true; }
-                break;
+//             switch( rule ){
+//                 case 'required':
+//                 if( i.val()==='' ){ ferror=ierror=true; }
+//                 break;
                 
-                case 'minlen':
-                if( i.val().length<parseInt(exp) ){ ferror=ierror=true; }
-                break;
-            }
-                i.next('.validation').html( ( ierror ? (i.attr('data-msg') != undefined ? i.attr('data-msg') : 'wrong Input') : '' ) ).show('blind');
-            }
-        });
-        if( ferror ) return false; 
-        else var str = $(this).serialize();		
-            $.ajax({
-                type: "POST",
-                url: "contactform/contactform.php",
-                data: str,
-                success: function(msg){
-                   // alert(msg);
-                    if(msg == 'OK') {
-                        $("#sendmessage").addClass("show");			
-                        $("#errormessage").removeClass("show");	
-                    }
-                    else {
-                        $("#sendmessage").removeClass("show");
-                        $("#errormessage").addClass("show");
-                        $('#errormessage').html(msg);
-                    }
+//                 case 'minlen':
+//                 if( i.val().length<parseInt(exp) ){ ferror=ierror=true; }
+//                 break;
+//             }
+//                 i.next('.validation').html( ( ierror ? (i.attr('data-msg') != undefined ? i.attr('data-msg') : 'wrong Input') : '' ) ).show('blind');
+//             }
+//         });
+//         if( ferror ) return false; 
+//         else var str = $(this).serialize();		
+//             $.ajax({
+//                 type: "POST",
+//                 url: "contactform/contactform.php",
+//                 data: str,
+//                 success: function(msg){
+//                    // alert(msg);
+//                     if(msg == 'OK') {
+//                         $("#sendmessage").addClass("show");			
+//                         $("#errormessage").removeClass("show");	
+//                     }
+//                     else {
+//                         $("#sendmessage").removeClass("show");
+//                         $("#errormessage").addClass("show");
+//                         $('#errormessage').html(msg);
+//                     }
                     
-                }
-            });
-        return false;
-    });
+//                 }
+//             });
+//         return false;
+//     });
 
-});
+// });
 
 // menu
 
-function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
-}
+// function openNav() {
+//   document.getElementById("mySidenav").style.width = "250px";
+// }
 
-function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
-}
-
-
+// function closeNav() {
+//   document.getElementById("mySidenav").style.width = "0";
+// }
 
 
-//  Google Maps
+
+
+// //  Google Maps
 
 
 
